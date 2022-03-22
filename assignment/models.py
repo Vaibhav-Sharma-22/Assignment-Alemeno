b581cb8f3d70545013bc78b297f165c57f816fc3
Vaibhav Sharma <vaibhavcool630@gmail.com>
2022-03-22 13:04:23 +0530

    Internship Task

--- assignment/models.py
+++ assignment/models.py
@@ -1,17 +1,54 @@
-from django.db import models
+from distutils.command.upload import upload
 
+from random import choice
+from sre_constants import SUCCESS
+from django.db import models
+from django.db.models.signals import post_save
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+FOOD_CHOICES = (
+    ("1", "Veg"),
+    ("2", "Fruit"),
+    ("3", "Grain"),
+    ("4", "Protein"),
+    ("5", "Dairy"),
+    ("6", "Confectionary"),
+    ("7", "Unknown")
+)
 # Create your models here.
+
+
+#Kid model
 class Kid(models.Model):
     Kid_Name = models.CharField('Name', max_length=100)
     Kid_Age = models.IntegerField('Age')
     Parent_Phone_Number = models.IntegerField(blank=True)
     Parent_Email_Address = models.EmailField('Email Address')
 
+    def __str__(self):
+        return self.Kid_Name
+    
+
+#Table model 
 class Image(models.Model):
-    Kid = models.ForeignKey(Kid, blank=True, null=True)
+    Kid = models.ForeignKey(Kid, blank=True, null=True, on_delete=models.CASCADE)
     Image_URL = models.URLField()
-    Created_On = models.DateTimeField()
+    Group = models.CharField(choices=FOOD_CHOICES, max_length=20, default='7')
+    Created_On = models.DateTimeField(auto_now_add=True)
     Updated_on = models.DateTimeField()
     Is_Approved = models.BooleanField()
-    Approved_by = models.CharField("", max_length=50)
-    FoodGroup = models.Choices
+    Updated_by = Kid.__getattribute__
+
+
+#Logic for Unknown food item mail send
+def image_post_save(*args, **kwargs):
+    instance = kwargs['instance']
+    if instance.Group=='7':
+        
+        send_mail('Unknow Food Item', 'Unknown Food Item in the Image', settings.EMAIL_HOST_USER, [instance.Kid.Parent_Email_Address])
+        pass
+    pass
+
+post_save.connect(image_post_save, sender=Image)
--- internship/settings.py
+++ internship/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m*t(@rqke7g!t0ajn)pde@rkv!u(!=y!xnd&j5y^*%r635uuz^'
+SECRET_KEY = config('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'assignment'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'internship.urls'
@@ -121,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', None)
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', None)
+
+django_heroku.settings(locals())
